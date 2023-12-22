1239b8616847b6dd99818e4f3fad38d5d053a864
evilimas <eg.vilimas@gmail.com>
2023-12-22 22:40:31 +0100

    fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import logo from './images/troll-face.png';
 export default function Header() {
   return (
     <header className="header">
-      <img src={logo} className="header--image" />
+      <img src={logo} className="header--image" alt="troll face logo img" />
       <h2 className="header--title">Your Meme Generator</h2>
       <h4 className="header--project">Make your favorite meme</h4>
     </header>
